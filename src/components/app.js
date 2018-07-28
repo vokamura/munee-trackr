f1ecee20da02f4ca932ec3eda7d0771054e970d6
vokamura <vcokamura@gmail.com>
2018-07-28 15:07:55 -0700

    Init commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+        <h1>Munee Trackr</h1>
     </div>
 );
 
