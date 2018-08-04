ca3c62776207bc615896f571a0fcd2e0580b7799
vokamura <vcokamura@gmail.com>
2018-08-04 10:53:46 -0700

    Delete not needed function and console log

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,18 +3,12 @@ import types from './types';
 export function updateExpenseLog(snapshot){
     const log = [];
 
-    // snapshot.forEach( doc => {
-    //     log.push(doc.data());
-    // });
-
     snapshot.forEach( document => {
         const doc = document.data();
         doc.id = document.id;
         
         log.push(doc);
     });
-
-    // console.log('Log in Action Creator', log);
 
     return {
         type: types.UPDATE_EXPENSE_LOG,
