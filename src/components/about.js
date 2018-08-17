c81033ec9c1f9c2accda6dd6d17aa03b6617f265
vokamura <vcokamura@gmail.com>
2018-08-17 12:15:35 -0700

    Move log page to home, and update about page

--- src/components/about.js
+++ src/components/about.js
@@ -5,14 +5,17 @@ export default () => {
         <div>
             <h1 className="center">About Munee Trackr</h1>
             <div className="center">
+
+                <p>Munee Trackr allows you to track income and expenses.</p>
+                
+                <p>It is a content management system, where you can see what entries have already been made, add new entries, update old entries, and delete entries.</p>
+
                 <p>Munee Trackr was created by Vikki Okamura to help users track personal expenses.  It was developed using ReactJS, Redux, Materialize, and Firebase.  This project was made as Vikki has a background in accounting and loves money.</p>
+
+
                 <div className="container">
-                    <div className="container-item row">
                         <a href="http://www.vikkiokamura.com" target="_blank" >Vikki's Portfolio</a>
-                    </div>
-                    <div className="container-item row">
                         <a href="https://github.com/vokamura" target="_blank">Vikki's Github</a>
-                    </div>
                 </div>
             </div>
         </div>
--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,6 @@ import 'materialize-css/dist/css/materialize.min.css';
 import '../assets/css/app.css';
 import { Route } from 'react-router-dom';
 import Nav from './nav';
-import Home from './home';
 import ExpenseLog from './expense_log';
 import About from './about';
 
@@ -11,8 +10,7 @@ const App = () => (
     <div>
         <Nav/>
         <div className="container">
-            <Route exact path="/" component={Home}/>
-            <Route path="/expense-log" component={ExpenseLog}/>
+            <Route exact path="/" component={ExpenseLog}/>
             <Route path="/about" component={About}/>
         </div>
     </div>
