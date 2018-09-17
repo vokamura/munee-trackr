ee29736ae921fed16b1dd4f9daa74cfb2b80f47a
vokamura <vcokamura@gmail.com>
2018-09-17 16:53:43 -0700

    Changed splash and form input modals to click anywhere to close

--- src/components/splash.js
+++ src/components/splash.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default (props) => {
     return (
-        <div onClick={props.hideModal} className={props.showSplash ? "modalSplash display-block" : "modalSplash display-none"}>
-            <div className="teal-text text-lighten-5 center-align" id="splashText">
-                <h2 className="teal-text text-lighten-2">Welcome to Munee Trackr!</h2>
-                <p className="splashInstructions ">Munee Trackr allows you to track income and expenses.</p>
+        <div onClick={props.hideModal} className={props.showSplash ? "splashShadow display-block" : "splashShadow display-none"}>
+            <div className="teal-text text-lighten-5 center-align splashBody" id="splashText">
+                <h2 className="teal-text text-lighten-2">Welcome to Munee Loggr!</h2>
+                <p className="splashInstructions ">Munee Loggr allows you to track income and expenses.</p>
                 <p className="splashInstructions">Please note that this is a demo app</p>
                 <p className="splashInstructions">Please note that data will be periodically wiped from the database</p>
-                <p className="splashInstructions teal-text text-lighten-2">Click anywhere in this box to close</p>
+                <p className="splashInstructions teal-text text-lighten-2">Click anywhere to close</p>
             </div>
         </div>
     )
