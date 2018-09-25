cdf0d83604b557822126abe400e7ac40b4388ebf
vokamura <vcokamura@gmail.com>
2018-09-25 13:18:03 -0700

    Took of Router and converted nav bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import '../assets/css/app.css';
-import { Route } from 'react-router-dom';
 import Nav from './nav';
 import ExpenseLog from './expense_log';
-import About from './about';
 
 const App = () => (
     <div>
         <Nav/>
         <div className="container">
-            <Route exact path="/" component={ExpenseLog}/>
-            <Route path="/about" component={About}/>
+            <ExpenseLog />
         </div>
     </div>
 );
