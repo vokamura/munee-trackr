dc03fccb476008891a8838a645d1d07a89786293
vokamura <vcokamura@gmail.com>
2018-09-25 19:26:02 -0700

    deployment

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: resolve(__dirname, 'dist'),
-        publicPath: '/dist/'
+        publicPath: '/'
     },
     context: resolve(__dirname, 'src'),
     module: {
